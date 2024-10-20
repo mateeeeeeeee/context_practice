aba8de99adad208ae0b351ce5deb1ca517f9ae93
mateeeeeeeee <mmate.pkakadze@gmail.com>
2024-10-20 15:54:28 +0400

    context

--- app/views.py
+++ app/views.py
@@ -5,3 +5,11 @@ def ip_address_procesor(request):
 
 def index(request):
     return render(request, 'index.html', ip_address_procesor(request))
+
+def user(request):
+    context = {
+        'name': 'mate',
+        'lastname': 'pkhakadze',
+        'age': '14'
+    }
+    return render(request, 'user.html', context=context)
